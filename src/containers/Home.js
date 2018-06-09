a654123bfceb1998e73ef4817c4914a25dda4b44
tronperidot <tronperidot@gmail.com>
2018-06-09 18:48:16 +0900

    テキストのみ本番で動いているサイトから内容を写しました。

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import { withSiteData } from 'react-static'
-//
-import logoImg from '../logo.png'
 
 export default withSiteData(() => (
   <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
-    <img src={logoImg} alt="" />
+    <h1 style={{ textAlign: 'center' }}>JUNK-TION!!</h1>
+    <p>2007年に小田和正、オフコースのトリビュートバンドとして活動を開始する。鎮目（しずめ）のハイトーンボイスが話題となり、各メディア、イベントに出演。2011年に入り、オリジナル曲の作成も開始、現在はトリビュートにメロディアスなオリジナル曲を取り混ぜたステージを提供している。</p>
+    <p>バンド経歴：<br />
+    ２００７年 小田和正、オフコースのトリビュートバンドとして活動を開始<br />
+    ２００７年 ＮＨＫ 熱血おやじバトル<br />
+    フジテレビ ものまね紅白歌合戦　（シズメのみ）<br />
+    ２００８年 日本テレビ 口コミジョニー（シズメのみ）<br />
+    ２００９年 ＮＨＫＢＳ２ 全国バンド自慢<br />
+    ２０１０年 テレビ東京 日経大人のバンド大賞　（オリジナル曲）<br />
+    ２０１１年 ＴＢＳ おもろげ動画ショー　（シズメのみ）<br />
+    フジテレビ クイズヘキサゴン　（シズメのみ）<br />
+    日本テレビ スッキリ<br />
+    ２０１２年 テレビ朝日 ＭｕｓｉｃるＴＶ（オリジナル曲）<br />
+    日本テレビ 世界一のショータイム<br />
+    ２０１３年 日本テレビ 世界一のショータイム</p>
+
+    <p>２０１１年〜２０１２年、東京ＦＭ系列のミュージックバードのＣＦＭ向けラジオ番組の<br />
+    パーソナリティを務める。同年にラジオ日本の番組もワンクール担当。同年２枚のオリジナルマキシシングルを発売。<br />
+    <br /></p>
+
+    <div class="fb-page fb_iframe_widget" data-href="http://facebook.com/junktion2007" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" fb-xfbml-state="rendered" fb-iframe-plugin-query="adapt_container_width=true&amp;app_id=&amp;container_width=585&amp;hide_cover=false&amp;href=http%3A%2F%2Ffacebook.com%2Fjunktion2007&amp;locale=ja_JP&amp;sdk=joey&amp;show_facepile=true&amp;small_header=false&amp;tabs=timeline">
+      <span style={{ 'vertical-align': 'bottom',  width: '340px', height: '500px' }}>
+        <iframe name="f11c00a3377418" width="1000px" height="1000px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" allow="encrypted-media" title="fb:page Facebook Social Plugin" src="https://www.facebook.com/v2.6/plugins/page.php?adapt_container_width=true&amp;app_id=&amp;channel=http%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter%2Fr%2FmAiQUwlReIP.js%3Fversion%3D42%23cb%3Df317f58faf1b95%26domain%3Djunk-tion.jp%26origin%3Dhttp%253A%252F%252Fjunk-tion.jp%252Ff28865e352aa41c%26relation%3Dparent.parent&amp;container_width=585&amp;hide_cover=false&amp;href=http%3A%2F%2Ffacebook.com%2Fjunktion2007&amp;locale=ja_JP&amp;sdk=joey&amp;show_facepile=true&amp;small_header=false&amp;tabs=timeline"
+          style={{ border: 'none', visibility: 'visible', width: '340px', height: '500px'}} class="">
+        </iframe>
+      </span>
+    </div>
   </div>
 ))
