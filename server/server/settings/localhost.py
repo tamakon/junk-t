1ce8a254b06e32cd65942bc1bb11dfeb04842812
h-haraguchi <account.cgtdnpck@me.com>
2018-08-25 22:40:30 +0900

    divide settings value by each environment
    チェックリストの一部を対応。
    https://docs.djangoproject.com/ja/2.1/howto/deployment/checklist/

--- server/server/settings/localhost.py
+++ server/server/settings/localhost.py
@@ -1,1 +1,9 @@
 from .base import *
+
+SECRET_KEY = 'c83jpzg11-&cby)65g20yze_6hxz3kw1%1p1czeln&l_x5@jl$'
+
+DEBUG = True
+ALLOWED_HOSTS = []
+
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
