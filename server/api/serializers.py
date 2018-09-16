5fbf51c94fdf1dc1830b9cc9dc0bcd4ec3762f9a
h-haraguchi <account.cgtdnpck@me.com>
2018-09-16 20:22:28 +0900

    add time field to Image model
    create_atとupdate_atを追加し、APIにupdate_atを追加。
    TimeZoneについてはDB上はUTCで保存し取得側(アプリ)でTokyoに変換するようにした。

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -6,6 +6,4 @@ from api.models import Image
 class ImageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Image
-        # Comma is needed because 'fields' field must be as tuple.
-        # If no comma, `fields` is converted to string type.
-        fields = ("tag",)
+        fields = ("tag", "update_at")
