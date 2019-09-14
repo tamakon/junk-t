d5f0f605c53d00a1e3dc937cc8f2e07d1a426af6
h-haraguchi <6197504+h-haraguchi@users.noreply.github.com>
2019-09-14 20:06:54 +0900

    add test case exception is thrown to unit test

--- server2/src/test/kotlin/junktion/v1/infrastructure/ImageFileStorageImplTest.kt
+++ server2/src/test/kotlin/junktion/v1/infrastructure/ImageFileStorageImplTest.kt
@@ -1,14 +1,21 @@
 package junktion.v1.infrastructure
 
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
+import junktion.v1.api.InfrastructureException
 import junktion.v1.core.Image
 import org.apache.tomcat.util.http.fileupload.FileUtils
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.core.io.ClassPathResource
+import org.springframework.util.FileCopyUtils
 import java.io.File
+import java.io.IOException
 
 internal class ImageFileStorageImplTest {
 
@@ -32,6 +39,18 @@ internal class ImageFileStorageImplTest {
     fun save() {
         val image = Image("test.png", ByteArray(0))
         imageFileStorage.save(image)
-        assertThat(File(ClassPathResource("static/images/test.png").uri).exists()).isTrue()
+        assertThat(File(ClassPathResource("static/images/").uri).resolve("test.png").exists()).isTrue()
+    }
+
+    @Test
+    @DisplayName("IO例外が発生した場合にインフラ関連例外にラップされること")
+    fun saveException() {
+        mockkStatic(FileCopyUtils::class)
+        every { FileCopyUtils.copy(any<ByteArray>(), any<File>()) } throws IOException()
+        val image = Image("test.png", ByteArray(0))
+        assertThatThrownBy { imageFileStorage.save(image) }
+                .isInstanceOf(InfrastructureException::class.java)
+        assertThat(File(ClassPathResource("static/images/").uri).resolve("test.png").exists()).isFalse()
+        unmockkStatic(FileCopyUtils::class)
     }
 }
--- server2/src/test/kotlin/junktion/v1/infrastructure/ImageRepositoryTest.kt
+++ server2/src/test/kotlin/junktion/v1/infrastructure/ImageRepositoryTest.kt
@@ -2,25 +2,36 @@ package junktion.v1.infrastructure
 
 import io.kotlintest.TestCase
 import io.kotlintest.specs.StringSpec
+import io.mockk.every
 import io.mockk.mockkClass
-import junktion.v1.api.ImageRepository
+import junktion.v1.api.InfrastructureException
 import junktion.v1.core.Image
 import junktion.v1.mybatis.ImageMapper
+import org.apache.ibatis.exceptions.PersistenceException
+import org.assertj.core.api.Assertions.assertThatThrownBy
 
 fun mockImageMapper() = mockkClass(ImageMapper::class, relaxed = true)
 
 class ImageRepositoryImplTest : StringSpec() {
 
-	private lateinit var imageRepository: ImageRepository
+	private lateinit var imageRepository: ImageRepositoryImpl
+	private lateinit var imageMapper: ImageMapper
 
 	override fun beforeTest(testCase: TestCase) {
-		imageRepository = ImageRepositoryImpl(mockImageMapper())
+		imageMapper = mockImageMapper()
+		imageRepository = ImageRepositoryImpl(imageMapper)
 	}
 
 	init {
 		"問題なく実行されること" {
 			val image = Image("tag", ByteArray(0))
 			imageRepository.register(image)
+		}
+		"永続化に関する例外が発生した場合に例外がインフラ関連例外としてラップされること" {
+			every { imageMapper.save(any()) } throws PersistenceException()
+			val image = Image("tag", ByteArray(0))
+			assertThatThrownBy { imageRepository.register(image) }
+					.isInstanceOf(InfrastructureException::class.java)
 		}
 	}
 }
