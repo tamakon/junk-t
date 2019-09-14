e3909dd38e62cfa634f1c423f4dad428dfe0205c
h-haraguchi <6197504+h-haraguchi@users.noreply.github.com>
2019-09-14 20:34:33 +0900

    add null pattern to image equality test

--- server2/src/test/kotlin/junktion/v1/core/ImageTest.kt
+++ server2/src/test/kotlin/junktion/v1/core/ImageTest.kt
@@ -24,12 +24,14 @@ internal class ImageTest {
         val theOtherName = createImage("different_name", ByteArray(0))
         val theOtherContent = createImage("image", ByteArray(1) { 1 })
         val theOtherInstance = "not a image" as Any?
+        val nullObject = null as Any?
 
         assertThat(criterion == criterion).isTrue()
         assertThat(criterion == sameFields).isTrue()
         assertThat(criterion == theOtherName).isFalse()
         assertThat(criterion == theOtherContent).isFalse()
         assertThat(criterion == theOtherInstance).isFalse()
+        assertThat(criterion == nullObject).isFalse()
     }
 
     @Test
